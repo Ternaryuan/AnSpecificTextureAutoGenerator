8260ea649973e027cecf29141049a9ab8f722647
Sanyuan <jli511@student.monash.edu>
2018-01-17 19:48:03 +1100

    main Structure

--- src/Generator.java
+++ src/Generator.java
@@ -5,8 +5,13 @@
 *
 */
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
 
 public class Generator extends JFrame{
 
@@ -21,24 +26,31 @@ public class Generator extends JFrame{
 
     public Generator(int xSize, int ySize){
         setTitle(MainTitle);
+        x = xSize - xSpace;
+        y = ySize - ySpace - yDisplacement;
         setSize(xSize, ySize);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         this.setResizable(false);
-        x = xSize - xSpace;
-        y = ySize - ySpace;
     }
 
     @Override
     public void paint(Graphics g){
-        int[] suitableMaxSize = new int[]{};
-        suitableMaxSize = getSuitableSizeMaxRect();
+        int[] suitableMaxSize = getSuitableSizeMaxRect();
+        int[] importantPointsX = new int[]{xDisplacement, xDisplacement + x, xDisplacement + x, xDisplacement, xDisplacement + (int)(x/2)};
+        int[] importantPointsY = new int[]{yDisplacement, yDisplacement, y + yDisplacement, y + yDisplacement, yDisplacement + (int)(y/2)};
+
         g.setColor(Color.red);
-
-
-
-
         g.drawRect(xDisplacement,yDisplacement,suitableMaxSize[0],suitableMaxSize[1]);
+
+        for (int i = 0; i < 4; i ++){
+            g.drawLine(importantPointsX[i],importantPointsY[i],importantPointsX[4],importantPointsY[4]);
+        }
+
+
+
+
+
 
         //Logs
         System.out.println(suitableMaxSize.length);
@@ -48,18 +60,18 @@ public class Generator extends JFrame{
 
     private static int[] getSuitableSizeMaxRect(){
         int[] result = null;
-        if (x == y * yFactor){
+        int yPlus = y * yFactor;
+        if (x == yPlus){
             result = new int[]{x,y};
         }
-        else if(x > y * yFactor){
-            result = new int[]{yFactor*y,y};
+        else if(x > yPlus){
+            result = new int[]{yPlus,y};
         }
-        else if(x < yFactor * 2){
+        else if(x < yPlus){
             result = new int[]{x,(int)(x/yFactor)};
         }
         return result;
     }
-
 
     public static void main(String[] args) {
         new Generator(1200 + xSpace,600 + ySpace + yDisplacement).setVisible(true);
